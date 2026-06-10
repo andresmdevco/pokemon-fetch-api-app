de1ca3e25f9992d89d00440eaaf575c7dd8182a2
Andrés Muñoz <andres2078@gmail.com>
2026-06-10 18:43:54 -0500

    feat: add main PokemonApp component

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+import { PokemonApp } from './pokemon/pokemon-app'
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
 <main>
   <h1 id="app-title">Hola Mundo</h1>
-  <section class="information-card">...</section>
+  <section class="information-card"></section>
 </main>
 `
+
+const element = document.querySelector('.information-card');
+
+PokemonApp(element);
---
+++ src/pokemon/pokemon-app.js
@@ -0,0 +1,17 @@
+/**
+ * Esta función crea la aplicación en el div enviado
+ * @param {HTMLDivElement} element 
+ */
+
+export const PokemonApp = (element) => {
+
+    document.title = 'Pokemon App';
+    const titleElement = document.querySelector('#app-title');
+
+    // if (titleElement) titleElement.innerHTML = 'Pokemon App';
+    titleElement && (titleElement.innerHTML = 'Pokemon App');
+    
+    console.log('Hola Mundo');
+    console.log(element);
+
+}
