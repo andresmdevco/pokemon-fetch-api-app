7a5cd2b38b4acc091d5f4145007e1e59b7b101ff
Andrés Muñoz <andres2078@gmail.com>
2026-06-10 22:49:41 -0500

    feat: simplify pokemon fetch action with async/await

--- src/pokemon/actions/get-pokemon-by-id.action.js
+++ src/pokemon/actions/get-pokemon-by-id.action.js
@@ -2,22 +2,34 @@
 /**
  * Fetch Pokemon information from PokeAPI
  * @param {number} id 
- * @returns {Object} Pokemon information
+ * @returns {Promise<Object>} Pokemon information
  */
-export const getPokemonById = (id) => {
+export const getPokemonById = async(id) => {
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        .then(response => {
-            // console.log({response});
-            // throw new Error('Pokemon no existe');
-            return response.json();
-        })
-        .then(data => {
-            console.log({data});
-        })
-        .catch(error => {
-            console.error(error);
-        });
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+    
+    const pokemonData = {
+        id: data.id,
+        name: data.name,
+        image: data.sprites.front_default
+    }
 
-    return {};
+    console.log({pokemonData});
+    return pokemonData;
+
+    // fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    //     .then(response => {
+    //         // console.log({response});
+    //         // throw new Error('Pokemon no existe');
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //         console.log({data});
+    //     })
+    //     .catch(error => {
+    //         console.error(error);
+    //     });
+
+    // return {};
 }
